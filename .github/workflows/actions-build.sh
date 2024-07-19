d990c006c6cafdbf9f9b2178325b8e75445d9aca
mlugg <mlugg@mlugg.co.uk>
2024-07-19 18:49:39 +0100

    ci: update to Zig 0.13.0

--- .github/workflows/actions-build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-zig build \
-	-Doptimize=ReleaseSafe -Dglslc="$PWD/shaderc/bin/glslc" \
-	-Dsuffix -Dstrip -Dtimestamp -Dtarget="$1"
-
--- .github/workflows/install-shaderc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-ci_page=https://storage.googleapis.com/shaderc/badges/build_link_linux_clang_release.html
-latest_build=$(curl -sSL "$ci_page" | sed 's/.*url=\([^"]*\)".*/\1/;q')
-curl -sSLo shaderc.tar.gz "$latest_build"
-tar -xzf shaderc.tar.gz
-mv install shaderc
