41701b4d039aa04b9e28a3255179369798801e63
InspectorBoat <inspectorofboats@gmail.com>
2025-11-26 01:45:08 -0500

    Clean up benchmarking code

--- src/bench.zig
+++ src/bench.zig
@@ -18,63 +18,16 @@ pub fn main() u8 {
 fn mainInternal() !void {
     var stdout = std.fs.File.stdout().writer(&.{});
     const w = &stdout.interface;
+
     try w.print("slimy benchmark v{f}\n\n", .{version.version});
 
     try benchCpu(w);
-    if (@import("build_consts").gpu_support) try benchGpu(w);
-}
 
-const test_seed: i64 = -2152535657050944081;
-const warmup_params: slimy.SearchParams = .{
-    .world_seed = test_seed,
-    .threshold = 39,
-
-    .x0 = -1000,
-    .z0 = -1000,
-    .x1 = 1000,
-    .z1 = 1000,
-
-    .method = undefined,
-};
-const warmup_results: []const slimy.Result = &.{
-    .{ .x = 949, .z = -923, .count = 43 },
-    .{ .x = 950, .z = -924, .count = 42 },
-    .{ .x = 245, .z = 481, .count = 40 },
-    .{ .x = 246, .z = 484, .count = 40 },
-    .{ .x = -624, .z = -339, .count = 40 },
-    .{ .x = 669, .z = -643, .count = 40 },
-    .{ .x = -623, .z = -701, .count = 40 },
-    .{ .x = 948, .z = -923, .count = 40 },
-    .{ .x = 949, .z = -924, .count = 40 },
-    .{ .x = 950, .z = -923, .count = 40 },
-    .{ .x = 950, .z = -926, .count = 40 },
-    .{ .x = 327, .z = -140, .count = 39 },
-    .{ .x = -423, .z = 50, .count = 39 },
-    .{ .x = 430, .z = 298, .count = 39 },
-    .{ .x = -554, .z = 270, .count = 39 },
-    .{ .x = 664, .z = 356, .count = 39 },
-    .{ .x = 715, .z = -375, .count = 39 },
-    .{ .x = 716, .z = -375, .count = 39 },
-    .{ .x = -309, .z = 800, .count = 39 },
-    .{ .x = -310, .z = 800, .count = 39 },
-    .{ .x = -726, .z = -575, .count = 39 },
-    .{ .x = -725, .z = -579, .count = 39 },
-    .{ .x = -726, .z = -579, .count = 39 },
-    .{ .x = 671, .z = -644, .count = 39 },
-    .{ .x = -624, .z = -701, .count = 39 },
-    .{ .x = -883, .z = 338, .count = 39 },
-    .{ .x = 684, .z = -752, .count = 39 },
-    .{ .x = -700, .z = 758, .count = 39 },
-    .{ .x = -636, .z = 843, .count = 39 },
-    .{ .x = 949, .z = -922, .count = 39 },
-    .{ .x = 951, .z = -923, .count = 39 },
-    .{ .x = 949, .z = -926, .count = 39 },
-    .{ .x = 951, .z = -924, .count = 39 },
-    .{ .x = 950, .z = -928, .count = 39 },
-};
-
-fn printBenchmarkHeader(out: *std.Io.Writer) !void {
-    try out.print("slimy benchmark v{f}\n\n", .{version.version});
+    try w.writeByte('\n');
+    if (@import("build_consts").gpu_support)
+        try benchGpu(w)
+    else
+        try w.print("skipping gpu benchmark; built without gpu support\n", .{});
 }
 
 fn printGpuHeader(out: *std.Io.Writer, gpu: slimy.gpu.Context) !void {
@@ -87,7 +40,7 @@ fn printGpuHeader(out: *std.Io.Writer, gpu: slimy.gpu.Context) !void {
         .other => "other",
         _ => "unknown",
     };
-    try out.print("GPU: {s} ({s})\n\n", .{ gpu_name, gpu_type });
+    try out.print("GPU: {s} ({s})\n", .{ gpu_name, gpu_type });
 }
 
 fn printCpuHeader(out: *std.Io.Writer) !void {
@@ -137,26 +90,31 @@ fn targetParams(rate: u128, target_secs: u8, method: slimy.SearchMethod) slimy.S
 }
 
 fn benchGpu(w: *std.Io.Writer) !void {
-    var gpu: slimy.gpu.Context = .{};
-    try gpu.init();
-    try printGpuHeader(w, gpu);
+    var gpu_context: slimy.gpu.Context = .{};
+    try gpu_context.init();
+    try printGpuHeader(w, gpu_context);
 
     var collector: Collector = .{};
     var params = warmup_params;
     params.method = .gpu;
     var timer: std.time.Timer = try .start();
 
-    try gpu.search(params, &collector, Collector.result, null);
+    try w.writeAll("Performing warmup test...");
+    try gpu_context.search(params, &collector, Collector.result, null);
+
     const approx_gpu_rate = locationRate(timer.read(), params);
-    try collector.check(warmup_results);
+    try w.writeAll(" Validating results...");
+    try collector.check(expected_warmup_results);
+    try w.writeAll(" OK\n");
+
     collector.reset();
 
     // 3x ~5s GPU benchmark
-    try w.writeAll("Testing GPU");
+    try w.writeAll("Performing 3x GPU runs");
     var gpu_rate: u128 = approx_gpu_rate;
     for (0..3) |i| {
         try w.writeByte('.');
-        gpu_rate += try benchGpuIter(&gpu, gpu_rate / (i + 1), 5);
+        gpu_rate += try benchGpuIter(&gpu_context, gpu_rate / (i + 1), 5);
     }
     gpu_rate = (gpu_rate - approx_gpu_rate) / 3; // Mean
     try w.print(
@@ -168,10 +126,10 @@ fn benchGpu(w: *std.Io.Writer) !void {
     );
 }
 
-fn benchGpuIter(gpu: *slimy.gpu.Context, rate: u128, target_secs: u8) !u128 {
+fn benchGpuIter(gpu_context: *slimy.gpu.Context, rate: u128, target_secs: u8) !u128 {
     const params = targetParams(rate, target_secs, .gpu);
-    var timer = try std.time.Timer.start();
-    try gpu.search(params, {}, devNull, null);
+    var timer: std.time.Timer = try .start();
+    try gpu_context.search(params, {}, devNull, null);
     return locationRate(timer.read(), params);
 }
 
@@ -179,21 +137,21 @@ fn benchCpu(w: *std.Io.Writer) !void {
     try printCpuHeader(w);
     var collector: Collector = .{};
     var params = warmup_params;
-    params.x0 = 0;
-    params.z0 = 0;
-    params.x1 = 100;
-    params.z1 = 100;
     params.method = .{ .cpu = std.math.lossyCast(u8, try std.Thread.getCpuCount()) };
-    params.method.cpu = 8;
     var timer: std.time.Timer = try .start();
+
+    try w.writeAll("Performing warmup test...");
     try slimy.cpu.search(params, &collector, Collector.result, null);
 
     const approx_cpu_rate = locationRate(timer.read(), params);
-    // try collector.check(warmup_results);
+    try w.writeAll(" Validating results...");
+    try collector.check(expected_warmup_results);
+    try w.writeAll(" OK\n");
+
     collector.reset();
 
-    // 3x ~5s GPU benchmark
-    try w.writeAll("Testing CPU");
+    // 3x ~5s CPU benchmark
+    try w.writeAll("Performing 3x CPU runs");
     var cpu_rate: u128 = approx_cpu_rate;
     for (0..3) |i| {
         try w.writeByte('.');
@@ -211,7 +169,7 @@ fn benchCpu(w: *std.Io.Writer) !void {
 
 fn benchCpuIter(threads: u8, rate: u128, target_secs: u8) !u128 {
     const params = targetParams(rate, target_secs, .{ .cpu = threads });
-    var timer = try std.time.Timer.start();
+    var timer: std.time.Timer = try .start();
     try slimy.cpu.search(params, {}, devNull, null);
     return locationRate(timer.read(), params);
 }
@@ -253,3 +211,52 @@ fn formatIntGrouped(
 fn devNull(_: void, res: slimy.Result) void {
     std.mem.doNotOptimizeAway(res);
 }
+
+const test_seed: i64 = -2152535657050944081;
+const warmup_params: slimy.SearchParams = .{
+    .world_seed = test_seed,
+    .threshold = 39,
+
+    .x0 = -1000,
+    .z0 = -1000,
+    .x1 = 1000,
+    .z1 = 1000,
+
+    .method = undefined,
+};
+const expected_warmup_results: []const slimy.Result = &.{
+    .{ .x = 949, .z = -923, .count = 43 },
+    .{ .x = 950, .z = -924, .count = 42 },
+    .{ .x = 245, .z = 481, .count = 40 },
+    .{ .x = 246, .z = 484, .count = 40 },
+    .{ .x = -624, .z = -339, .count = 40 },
+    .{ .x = 669, .z = -643, .count = 40 },
+    .{ .x = -623, .z = -701, .count = 40 },
+    .{ .x = 948, .z = -923, .count = 40 },
+    .{ .x = 949, .z = -924, .count = 40 },
+    .{ .x = 950, .z = -923, .count = 40 },
+    .{ .x = 950, .z = -926, .count = 40 },
+    .{ .x = 327, .z = -140, .count = 39 },
+    .{ .x = -423, .z = 50, .count = 39 },
+    .{ .x = 430, .z = 298, .count = 39 },
+    .{ .x = -554, .z = 270, .count = 39 },
+    .{ .x = 664, .z = 356, .count = 39 },
+    .{ .x = 715, .z = -375, .count = 39 },
+    .{ .x = 716, .z = -375, .count = 39 },
+    .{ .x = -309, .z = 800, .count = 39 },
+    .{ .x = -310, .z = 800, .count = 39 },
+    .{ .x = -726, .z = -575, .count = 39 },
+    .{ .x = -725, .z = -579, .count = 39 },
+    .{ .x = -726, .z = -579, .count = 39 },
+    .{ .x = 671, .z = -644, .count = 39 },
+    .{ .x = -624, .z = -701, .count = 39 },
+    .{ .x = -883, .z = 338, .count = 39 },
+    .{ .x = 684, .z = -752, .count = 39 },
+    .{ .x = -700, .z = 758, .count = 39 },
+    .{ .x = -636, .z = 843, .count = 39 },
+    .{ .x = 949, .z = -922, .count = 39 },
+    .{ .x = 951, .z = -923, .count = 39 },
+    .{ .x = 949, .z = -926, .count = 39 },
+    .{ .x = 951, .z = -924, .count = 39 },
+    .{ .x = 950, .z = -928, .count = 39 },
+};
